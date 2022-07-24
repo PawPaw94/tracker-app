9d5706b65b52f66c4c806ddf8a89b3287317bb5b
PawPaw94 <p.pawlikowski94@gmail.com>
2022-07-24 12:58:31 +0300

    Updates

---
+++ precache-manifest.bb6b9fc8c39a9556bd98768463def35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec69d50e07a60609a42",
+    "url": "/tracker-app.git/static/css/main.bd7941c1.chunk.css"
+  },
+  {
+    "revision": "2ec69d50e07a60609a42",
+    "url": "/tracker-app.git/static/js/main.2ec69d50.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/tracker-app.git/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "40538af08661e826c7f1",
+    "url": "/tracker-app.git/static/js/2.40538af0.chunk.js"
+  },
+  {
+    "revision": "f77f4b5e97ce976b3d3d",
+    "url": "/tracker-app.git/static/js/runtime~main.f77f4b5e.js"
+  },
+  {
+    "revision": "ce649decfae920519102e3f16e7be358",
+    "url": "/tracker-app.git/index.html"
+  }
+];
